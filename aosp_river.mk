b5bd964fbecc2ec279566d6516ce4b5a0295e8c5
vulkan-ops <vulkanops@gmail.com>
2021-06-02 22:23:14 -0300

    river: enable adb , disable oficial build
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- aosp_river.mk
+++ aosp_river.mk
@@ -22,7 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common Kraken stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-CUSTOM_BUILD_TYPE := OFFICIAL
+#CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_ENABLE_ADB := true
 
 # Inherit from river device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
