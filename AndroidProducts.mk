3b811fc79c0530ff083e0bd9eb041c54d493d033
vulkan-ops <vulkanops@gmail.com>
2021-07-01 18:01:19 -0300

    river: lineageOS-ify
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_river.mk
+    $(LOCAL_DIR)/lineage_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_river-eng \
-    aosp_river-user \
-    aosp_river-userdebug
+    lineage_river-eng \
+    lineage_river-user \
+    lineage_river-userdebug
